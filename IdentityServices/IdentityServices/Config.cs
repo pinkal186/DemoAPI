fa744a0ce2a2a98b4e1f2e7dc55831ebefb26a6f
Pinkal Prajapati <pinkal.prajapati@gmail.com>
2018-06-26 10:06:24 +0530

    Starting Project
    Starting Project

---
+++ IdentityServices/IdentityServices/Config.cs
@@ -0,0 +1,55 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServices
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "vivek",
+                    Password = "password"
+                }
+
+            };
+        }
+
+    }
+}
